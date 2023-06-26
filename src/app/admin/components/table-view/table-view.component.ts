db0b3158bde6f62e26da72134b561b62078c0931
CristiStroescu <116139196+CristiStroescu@users.noreply.github.com>
2023-06-26 14:48:55 +0300

    Created a dropdown list for sorting the list for Table-View Component from Admin Module.

--- src/app/admin/components/table-view/table-view.component.ts
+++ src/app/admin/components/table-view/table-view.component.ts
@@ -5,23 +5,54 @@ import { DishService } from 'src/app/user/services/dish.service';
 @Component({
   selector: 'app-table-view',
   templateUrl: './table-view.component.html',
-  styleUrls: ['./table-view.component.scss']
+  styleUrls: ['./table-view.component.scss'],
 })
 export class TableViewComponent {
-  dishes!:Dish[];
+  dishes!: Dish[];
 
-  constructor(private dishService:DishService){}
+  selectedSortOption: string = 'none';
+
+  constructor(private dishService: DishService) {}
 
   ngOnInit(): void {
-    this.dishes=this.dishService.arrayOfDishes;
+    this.dishes = this.dishService.arrayOfDishes;
   }
 
-  addNewItem(){
-  }
+  addNewItem() {}
 
-  editItem(dish: Dish) {
-  }
+  editItem(dish: Dish) {}
 
-  deleteItem(){
+  deleteItem() {}
+  get sortedItems(): any[] {
+    switch (this.selectedSortOption) {
+      case 'none':
+        return this.dishes;
+      case 'priceL-H':
+        return this.dishes.slice().sort((a, b) => a.price - b.price);
+      case 'priceH-L':
+        return this.dishes.slice().sort((a, b) => b.price - a.price);
+      case 'nameA-Z':
+        return this.dishes.slice().sort((a, b) => a.name.localeCompare(b.name));
+      case 'nameZ-A':
+        return this.dishes.slice().sort((a, b) => b.name.localeCompare(a.name));
+      case 'categoryA-Z':
+        return this.dishes
+          .slice()
+          .sort((a, b) => a.category.localeCompare(b.category));
+      case 'categoryZ-A':
+        return this.dishes
+          .slice()
+          .sort((a, b) => b.category.localeCompare(a.category));
+      case 'portionSizeL-H':
+        return this.dishes
+          .slice()
+          .sort((a, b) => a.portionSize - b.portionSize);
+      case 'portionSizeH-L':
+        return this.dishes
+          .slice()
+          .sort((a, b) => b.portionSize - a.portionSize);
+      default:
+        return this.dishes;
+    }
   }
 }
